db1fe228a0b7119306cf1f6967b5da102bb73099
John Huynh <jhnhnyh9@gmail.com>
2021-12-06 13:13:37 -0800

    initial commit, add app template with header and footer

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
+</script>
+
+<Header />
+<main>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam pariatur,
+    consequuntur placeat, ab nulla suscipit ipsum optio, accusantium natus velit
+    quos maxime deserunt nobis deleniti numquam ea excepturi voluptatem magnam.
+  </p>
+</main>
+<Footer />
+
+<style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
---
+++ src/components/Footer.svelte
@@ -0,0 +1,30 @@
+<footer>
+  <div class="copyright">Copyright Crowd Polls</div>
+
+  <div class="icon-attribute">
+    Icons made by <a
+      href="https://www.flaticon.com/authors/freepik"
+      title="Freepik">Freepik</a
+    >
+    from
+    <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+  </div>
+</footer>
+
+<style>
+  footer {
+    padding: 40px;
+    text-align: center;
+  }
+  .copyright {
+    color: #aaa;
+    font-size: 14px;
+    padding: 20px;
+    border-top: 1px solid #ddd;
+  }
+  .icon-attribute {
+    color: #aaa;
+    font-size: 14px;
+    padding: 5px;
+  }
+</style>
