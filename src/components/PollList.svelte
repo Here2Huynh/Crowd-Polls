cb5bba343d5acc9c29ae2dfe273048d666b29b09
John Huynh <jhnhnyh9@gmail.com>
2021-12-11 13:49:25 -0800

    implement card component

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,10 +1,13 @@
 <script>
   export let polls = [];
+  import PollDetails from "./PollDetails.svelte";
 </script>
 
 <div class="poll-list">
   {#each polls as poll (poll.id)}
-    <div>{poll.question}</div>
+    <div>
+      <PollDetails {poll} />
+    </div>
   {/each}
 </div>
 
